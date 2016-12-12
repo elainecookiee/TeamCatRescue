e4aaf82fa32e26cb2671076f2a32d0eac2034d30
Amnon <amnon.skladman@gmail.com>
2016-12-12 15:37:32 -0500

    Header

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Team Cat Rescue</title> 
+        <meta charset=utf-8> 
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet"> 
+        <!-- The browser will automatically choose the appropriate FavIcon size. -->
+        <link rel="icon" type="image/png" href="assets/fav16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="assets/fav32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="assets/fav48.png" sizes="48x48">
+    </head>
+    <body>
+        <header> 
+            <nav>
+                <img id="mainLogo" src="assets/logo.png" />
+                <a href="index.php" target="_self" class="topNav">Home</a> <a href="about.html" target="_self" class="topNav">About</a> <a href="catcare.html" target="_self" class="topNav">Cat Care</a> <a href="contact_page.html" target="_self" class="topNav">Contact</a>
+            </nav>
+        </header>
